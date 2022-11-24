982fb868a0f7cce69c1b15484a6c5b454bea55f5
AnandJNairGit <anandjnairit@gmail.com>
2022-11-24 23:33:34 +0530

    add form validation using yup

--- src/pages/authPage/index.js
+++ src/pages/authPage/index.js
@@ -3,7 +3,8 @@
 import { Button, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 import { Field, Form, Formik } from "formik";
-
+import { useEffect } from "react";
+import * as yup from "yup";
 const initialValues = {
   name: "",
   email: "",
@@ -13,14 +14,36 @@ const initialValues = {
 
 const formikProps = {
   initialValues: initialValues,
-  onSubmit: (values, formikHelpers) => {console.log("the values are:", values);},
+  onSubmit: (values, formikHelpers) => {
+    console.log("the values are:", values);
+  },
+  validationSchema: yup.object({
+    name: yup.string().required("name is required"),
+    email: yup.string().required("email is required"),
+    password: yup
+      .string()
+      .required("password is required")
+      .matches(
+        /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/,
+        "Please enter a strong password"
+      ),
+    confirmPassword: yup
+      .string()
+      .required("confirmPassword is required")
+      .oneOf([yup.ref("password"), null], "Passwords must match"),
+  }),
 };
 
 const AuthPage = () => {
+  useEffect(() => {
+    console.log("rendering");
+  });
+
   return (
     <div className="auth">
       <Formik {...formikProps}>
-        {() => {
+        {({ errors, isValid, touched }) => {
+          console.log(errors);
           return (
             <Form>
               <Field
