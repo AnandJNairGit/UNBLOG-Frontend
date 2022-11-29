ee897d9d3c49f7baa9be1e67280fcf474aa78a89
AnandJNairGit <anandjnairit@gmail.com>
2022-11-29 23:55:34 +0530

    use signup component in auth page

--- src/pages/authPage/index.js
+++ src/pages/authPage/index.js
@@ -1,43 +1,12 @@
 // import React from "react";
 
-import { Button, Grid, TextField, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import { Field, Form, Formik } from "formik";
 import { useEffect } from "react";
-import * as yup from "yup";
 
 import logo from "../../assets/logo/UNBLOG.png";
 import authPic from "../../assets/Authenticate/Secure.svg";
-import TextInput from "../../common/components/TextInput";
-
-const initialValues = {
-  name: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
-};
-
-const formikProps = {
-  initialValues: initialValues,
-  onSubmit: (values, formikHelpers) => {
-    console.log("the values are:", values);
-  },
-  validationSchema: yup.object({
-    name: yup.string().required("name is required"),
-    email: yup.string().required("email is required").email(),
-    password: yup
-      .string()
-      .required("password is required")
-      .matches(
-        /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/,
-        "Please enter a strong password"
-      ),
-    confirmPassword: yup
-      .string()
-      .required()
-      .oneOf([yup.ref("password"), null], "Passwords must match"),
-  }),
-};
+import SignUp from "./SignUp";
 
 const AuthPage = () => {
   useEffect(() => {
@@ -72,58 +41,21 @@ const AuthPage = () => {
         </Grid>
         <Grid item md={12} lg={6}>
           {/* AUTH SECTION */}
-          <Box sx={{ maxWidth: "sm", minWidth: "600px", padding: 4 }}>
-            <div className="neumorphic p-5">
-              <img src={logo}></img>
-              <Formik {...formikProps}>
-                {({ errors, isValid, touched }) => {
-                  console.log(touched);
-                  return (
-                    <Form>
-                      <TextInput
-                        name="name"
-                        label="name"
-                        type="name"
-                        error={errors.name}
-                        touched={touched.name}
-                      />
-
-                      <TextInput
-                        name="email"
-                        label="email"
-                        type="email"
-                        error={errors.email}
-                        touched={touched.email}
-                      />
-
-                      <TextInput
-                        name="password"
-                        label="password"
-                        type="password"
-                        error={errors.password}
-                        touched={touched.password}
-                      />
-                      <TextInput
-                        name="confirmPassword"
-                        label="confirm password"
-                        type="confirmPassword"
-                        error={errors.confirmPassword}
-                        touched={touched.confirmPassword}
-                      />
-
-                      <Button
-                        fullWidth
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                        size="large"
-                      >
-                        Sign up
-                      </Button>
-                    </Form>
-                  );
+          <Box>
+            <div className="neumorphic p-10">
+              <Box
+                sx={{
+                  maxWidth: "500px",
+                  minWidth: "sm",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignContent: "center",
                 }}
-              </Formik>
+              >
+                <img width="150px" src={logo}></img>
+                <SignUp />
+              </Box>
             </div>
           </Box>
         </Grid>
